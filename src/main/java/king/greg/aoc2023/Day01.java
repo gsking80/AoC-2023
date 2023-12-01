f117887b132f3c5e5df1b4a9e86f2283ab7a7019
Greg King <greg.king@gfs.com>
2023-12-01 01:17:34 -0500

    Day 01

--- src/main/java/king/greg/aoc2023/Day01.java
+++ src/main/java/king/greg/aoc2023/Day01.java
@@ -1,8 +1,32 @@
 package king.greg.aoc2023;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day01 {
+
+  private static final Map<String, Integer> numbers = new HashMap<>();
+  static {
+    numbers.put("1", 1);
+    numbers.put("2", 2);
+    numbers.put("3", 3);
+    numbers.put("4", 4);
+    numbers.put("5", 5);
+    numbers.put("6", 6);
+    numbers.put("7", 7);
+    numbers.put("8", 8);
+    numbers.put("9", 9);
+    numbers.put("one", 1);
+    numbers.put("two", 2);
+    numbers.put("three", 3);
+    numbers.put("four", 4);
+    numbers.put("five", 5);
+    numbers.put("six", 6);
+    numbers.put("seven", 7);
+    numbers.put("eight", 8);
+    numbers.put("nine", 9);
+  }
 
   private final List<String> lines;
 
@@ -10,8 +34,54 @@ public class Day01 {
     this.lines = lines;
   }
 
-  public List<String> getLines() {
-    return lines;
+  public long calibrate() {
+    var value = 0;
+
+    for (final var line: lines){
+      for (var i = 0; i < line.length(); i++) {
+        final var character = line.charAt(i);
+        if (character >= '0' && character <= '9') {
+          value += (character - '0') * 10;
+          break;
+        }
+      }
+      for (var i = line.length() - 1; i >= 0; i--) {
+        final var character = line.charAt(i);
+        if (character >= '0' && character <= '9') {
+          value += character - '0';
+          break;
+        }
+      }
+    }
+
+    return value;
+  }
+
+  public long calibrateWithWords() {
+    var value = 0;
+
+    for (final var line: lines) {
+      var firstIndex = line.length();
+      var lastIndex = -1;
+      var tens = 0;
+      var ones = 0;
+      for(final var entry : numbers.entrySet()) {
+        final var digit = entry.getKey();
+        var findFirst = line.indexOf(digit);
+        if (findFirst != -1 && findFirst < firstIndex) {
+          firstIndex = findFirst;
+          tens = numbers.get(digit);
+        }
+        var findLast = line.lastIndexOf(digit);
+        if(findLast != -1 && findLast > lastIndex) {
+          lastIndex = findLast;
+          ones = numbers.get(digit);
+        }
+      }
+      value += (tens * 10) + ones;
+    }
+
+    return value;
   }
 
 }
--- src/test/java/king/greg/aoc2023/Day01Test.java
+++ src/test/java/king/greg/aoc2023/Day01Test.java
@@ -11,12 +11,39 @@ import org.junit.jupiter.api.Test;
 class Day01Test {
 
   @Test
+  void testSample1() throws URISyntaxException, IOException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day01/sample1.txt"))
+            .toURI()));
+    final Day01 day01 = new Day01(lines);
+    Assertions.assertThat(day01.calibrate()).isEqualTo(142);
+  }
+
+  @Test
   void testSolution1() throws URISyntaxException, IOException {
     final var lines = Files.readAllLines(Paths.get(
         Objects.requireNonNull(getClass().getClassLoader().getResource("Day01/input.txt"))
             .toURI()));
     final Day01 day01 = new Day01(lines);
-    Assertions.assertThat(day01.getLines()).containsExactly("123", "test");
+    Assertions.assertThat(day01.calibrate()).isEqualTo(55712);
+  }
+
+  @Test
+  void testSample2() throws URISyntaxException, IOException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day01/sample2.txt"))
+            .toURI()));
+    final Day01 day01 = new Day01(lines);
+    Assertions.assertThat(day01.calibrateWithWords()).isEqualTo(281);
+  }
+
+  @Test
+  void testSolution2() throws URISyntaxException, IOException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day01/input.txt"))
+            .toURI()));
+    final Day01 day01 = new Day01(lines);
+    Assertions.assertThat(day01.calibrateWithWords()).isEqualTo(55413);
   }
 
 }
