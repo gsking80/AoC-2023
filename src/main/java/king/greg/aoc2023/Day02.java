d9f555ae9c64ccfef590f63e468e9971628c498a
Greg King <greg.king@gfs.com>
2023-12-02 07:49:30 -0500

    Day 02 the cleaner way!  We don't need to actually differentiate between pulls.

--- src/main/java/king/greg/aoc2023/Day02.java
+++ src/main/java/king/greg/aoc2023/Day02.java
@@ -12,88 +12,52 @@ public class Day02 {
 
   public int possibleGamesScore(final int redCubes, final int greenCubes, final int blueCubes) {
     var possibleGamesScore = 0;
-    for (final var game : lines) {
-      final var parts = game.split(":");
-      final var gameId = Integer.parseInt(parts[0].split(" ")[1]);
-      final var pulls = parts[1].split(";");
-      var validGame = true;
-      for (final var pull : pulls) {
-        final var colors = pull.split(",");
-        for (final var color : colors) {
-          final var colorPieces = color.split(" ");
-          final var count = Integer.parseInt(colorPieces[1]);
-          switch (colorPieces[2]) {
-            case "red" -> {
-              if (count > redCubes) {
-                validGame = false;
-              }
-            }
-            case "green" -> {
-              if (count > greenCubes) {
-                validGame = false;
-              }
-            }
-            case "blue" -> {
-              if (count > blueCubes) {
-                validGame = false;
-              }
-            }
-            default -> throw new IllegalArgumentException(colorPieces[2]);
-          }
-          if (!validGame) {
-            break;
-          }
-        }
-        if (!validGame) {
-          break;
-        }
-      }
-      if (validGame) {
-        possibleGamesScore += gameId;
+    for (final var line : lines) {
+      final var game = line.split("[:, ;]+");
+      final var colorMax = colorMax(game);
+      if (colorMax[0] <= redCubes && colorMax[1] <= greenCubes && colorMax[2] <= blueCubes) {
+        possibleGamesScore += Integer.parseInt(game[1]);
       }
     }
-
     return possibleGamesScore;
   }
 
   final long powerSum() {
     long powerSum = 0;
 
-    for (final var game : lines) {
-      final var parts = game.split(":");
-      final var pulls = parts[1].split(";");
-      var minRed = 0;
-      var minGreen = 0;
-      var minBlue = 0;
-      for (final var pull : pulls) {
-        final var colors = pull.split(",");
-        for (final var color : colors) {
-          final var colorPieces = color.split(" ");
-          final var count = Integer.parseInt(colorPieces[1]);
-          switch (colorPieces[2]) {
-            case "red" -> {
-              if (count > minRed) {
-                minRed = count;
-              }
-            }
-            case "green" -> {
-              if (count > minGreen) {
-                minGreen = count;
-              }
-            }
-            case "blue" -> {
-              if (count > minBlue) {
-                minBlue = count;
-              }
-            }
-            default -> throw new IllegalArgumentException(colorPieces[2]);
-          }
-        }
-      }
-      powerSum += (long) minRed * minGreen * minBlue;
+    for (final var line : lines) {
+      final var game = line.split("[:, ;]+");
+      final var colorMax = colorMax(game);
+      powerSum += (long) colorMax[0] * colorMax[1] * colorMax[2];
     }
 
     return powerSum;
+  }
+
+  private int[] colorMax(final String[] game) {
+    final int[] colorMax = new int[3];
+    for (var i = 2; i < game.length; i+=2){
+      final int count = Integer.parseInt(game[i]);
+      switch (game[i+1]) {
+        case "red" -> {
+          if (count > colorMax[0]) {
+            colorMax[0] = count;
+          }
+        }
+        case "green" -> {
+          if (count > colorMax[1]) {
+            colorMax[1] = count;
+          }
+        }
+        case "blue" -> {
+          if (count > colorMax[2]) {
+            colorMax[2] = count;
+          }
+        }
+        default -> throw new IllegalArgumentException(game[i+1]);
+      }
+    }
+    return colorMax;
   }
 
 }
