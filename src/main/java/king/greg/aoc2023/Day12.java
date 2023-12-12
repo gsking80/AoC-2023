7a62ee1a77f92054a50fe2c1256514f1f2eacac1
Greg King <greg.king@gfs.com>
2023-12-12 05:56:59 -0500

    Day 12 part 2 featuring Dynamic Programming finally working!

--- src/main/java/king/greg/aoc2023/Day12.java
+++ src/main/java/king/greg/aoc2023/Day12.java
@@ -1,61 +1,101 @@
 package king.greg.aoc2023;
 
-import java.util.ArrayDeque;
+import java.awt.Point;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Deque;
+import java.util.HashMap;
 import java.util.List;
-import org.apache.commons.lang3.StringUtils;
+import java.util.Map;
 
 public class Day12 {
 
   private final List<String> lines;
+  private final Map<Point, Long> memo;
 
   Day12(final List<String> lines) {
     this.lines = lines;
+    memo = new HashMap<>();
   }
 
-  public int totalArrangements() {
-    int totalArrangements = 0;
+  public long totalArrangements(final int folds) {
+    long totalArrangements = 0;
     for (final var line : lines) {
-      totalArrangements += arrangements(line);
+      totalArrangements += arrangements(line, folds);
     }
     return totalArrangements;
   }
 
-  private static int arrangements(final String line) {
-    int arrangements = 0;
+  private long arrangements(final String line, final int folds) {
+    memo.clear();
     final var parts = line.split(" ");
-    final var damages = Arrays.stream(parts[1].split(",")).map(Integer::valueOf).toList();
-    final Deque<String> possibilities = new ArrayDeque<>();
-    possibilities.add(parts[0]);
-    while(!possibilities.isEmpty()) {
-      final var current = possibilities.removeFirst();
-      final var firstDecision = current.indexOf('?');
-      if(firstDecision == -1) {
-        if(validConfiguration(current, damages)){
-          arrangements++;
-        }
-      } else {
-        possibilities.add(current.replaceFirst("\\?", "."));
-        possibilities.add(current.replaceFirst("\\?", "#"));
+    final var foldedDamages = Arrays.stream(parts[1].split(",")).map(Integer::valueOf).toList();
+    final StringBuilder springsBuilder = new StringBuilder();
+    final List<Integer> damages = new ArrayList<>();
+    for (var i = 0; i < folds - 1; i++) {
+      springsBuilder.append(parts[0]).append("?");
+      damages.addAll(foldedDamages);
+    }
+    springsBuilder.append(parts[0]);
+    damages.addAll(foldedDamages);
+
+    return countArrangements(springsBuilder.toString(), 0, damages, 0);
+  }
+
+  private long countArrangements(final String springs, final int springIndex,
+      final List<Integer> damages, final int damageIndex) {
+    if (damageIndex == damages.size()) {
+      return 1;
+    }
+    var memoed = memo.get(new Point(springIndex, damageIndex));
+    if (null != memoed) {
+      return memoed;
+    }
+
+    long count = 0;
+    for (int i = springIndex; i <= springs.length() - damages.get(damageIndex); i++) {
+
+      if (rightBoundaryValid(springs, i, damages, damageIndex) &&
+          interiorSequenceValid(springs, i, damages, damageIndex) &&
+          endValid(springs, i, damages, damageIndex)) {
+        count += countArrangements(springs, i + damages.get(damageIndex) + 1, damages,
+            damageIndex + 1);
+      }
+
+      if (springs.charAt(i) == '#') { // left side is locked
+        break;
       }
     }
 
+    memo.put(new Point(springIndex, damageIndex), count);
 
-    return arrangements;
+    return count;
   }
 
-  private static boolean validConfiguration(final String configuration, final List<Integer> damages) {
-    final var sections = StringUtils.split(configuration, '.');
-    if (sections.length != damages.size()) {
-      return false;
-    }
-    for(int i = 0; i < sections.length; i++) {
-      if(sections[i].length() != damages.get(i)) {
+  private boolean rightBoundaryValid(final String springs, final int springIndex,
+      final List<Integer> damages, final int damageIndex) {
+    return !(springIndex + damages.get(damageIndex) < springs.length()
+        && springs.charAt(springIndex + damages.get(damageIndex)) == '#');
+  }
+
+  private boolean interiorSequenceValid(final String springs, final int springIndex,
+      final List<Integer> damages, final int damageIndex) {
+    for (int i = 0; i < damages.get(damageIndex); i++) {
+      if (springs.charAt(springIndex + i) == '.') {
         return false;
       }
     }
     return true;
   }
 
+  private boolean endValid(final String springs, final int springIndex, final List<Integer> damages,
+      final int damageIndex) {
+    if (damageIndex == damages.size() - 1) {
+      for (int i = springIndex + damages.get(damageIndex) + 1; i < springs.length(); i++) {
+        if (springs.charAt(i) == '#') {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 }
--- src/test/java/king/greg/aoc2023/Day12Test.java
+++ src/test/java/king/greg/aoc2023/Day12Test.java
@@ -1,7 +1,5 @@
 package king.greg.aoc2023;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
@@ -18,7 +16,7 @@ class Day12Test {
         Objects.requireNonNull(getClass().getClassLoader().getResource("Day12/sample1.txt"))
             .toURI()));
     final Day12 day12 = new Day12(lines);
-    Assertions.assertThat(day12.totalArrangements()).isEqualTo(21);
+    Assertions.assertThat(day12.totalArrangements(1)).isEqualTo(21);
   }
 
   @Test
@@ -27,7 +25,24 @@ class Day12Test {
         Objects.requireNonNull(getClass().getClassLoader().getResource("Day12/input.txt"))
             .toURI()));
     final Day12 day12 = new Day12(lines);
-    Assertions.assertThat(day12.totalArrangements()).isEqualTo(7236);
+    Assertions.assertThat(day12.totalArrangements(1)).isEqualTo(7236);
   }
 
+  @Test
+  void testSample2() throws URISyntaxException, IOException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day12/sample1.txt"))
+            .toURI()));
+    final Day12 day12 = new Day12(lines);
+    Assertions.assertThat(day12.totalArrangements(5)).isEqualTo(525152);
+  }
+
+  @Test
+  void testSolution2() throws URISyntaxException, IOException {
+    final var lines = Files.readAllLines(Paths.get(
+        Objects.requireNonNull(getClass().getClassLoader().getResource("Day12/input.txt"))
+            .toURI()));
+    final Day12 day12 = new Day12(lines);
+    Assertions.assertThat(day12.totalArrangements(5)).isEqualTo(11607695322318L);
+  }
 }
