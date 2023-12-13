04e4e67764484be2fc228d017ca8f06dbe51e356
Greg King <greg.king@gfs.com>
2023-12-13 12:10:06 -0500

    Day 13 - accounting for an edge case that didn't actually exist and other cleanup

--- src/main/java/king/greg/aoc2023/Day13.java
+++ src/main/java/king/greg/aoc2023/Day13.java
@@ -10,48 +10,51 @@ import org.apache.commons.lang3.tuple.Pair;
 
 public class Day13 {
 
-  private final List<Set<Point>> maps;
+  private final List<Pair<Set<Point>, Point>> maps;
 
   Day13(final List<String> lines) {
     maps = new ArrayList<>();
     Set<Point> currentMap = new HashSet<>();
-    int y = 0;
+    int y = -1;
+    int maxX = 0;
     for (final var line : lines) {
       if (line.isBlank()) {
-        maps.add(currentMap);
+        maps.add(Pair.of(currentMap, new Point(maxX, y)));
         currentMap = new HashSet<>();
-        y = 0;
+        y = -1;
       } else {
+        y++;
         for (int x = 0; x < line.length(); x++) {
           if (line.charAt(x) == '#') {
             currentMap.add(new Point(x, y));
           }
+          maxX = x;
         }
-        y++;
       }
     }
-    maps.add(currentMap);
+    maps.add(Pair.of(currentMap, new Point(maxX, y)));
   }
 
   public int totalReflectionScore() {
     int reflectionScore = 0;
-    for (final var map : maps) {
-      reflectionScore += reflectionScore(map);
+    for (final var mapEntry : maps) {
+      reflectionScore += reflectionScore(mapEntry);
     }
     return reflectionScore;
   }
 
   public int totalSmudgeReflectionScore() {
     int reflectionScore = 0;
-    for (final var map : maps) {
-      reflectionScore += smudgeReflectionScore(map);
+    for (final var mapEntry : maps) {
+      reflectionScore += smudgeReflectionScore(mapEntry);
     }
     return reflectionScore;
   }
 
-  private int reflectionScore(final Set<Point> map) {
-    final int xMax = map.stream().mapToInt(p -> p.x).max().orElse(0);
-    final int yMax = map.stream().mapToInt(p -> p.y).max().orElse(0);
+  private int reflectionScore(final Pair<Set<Point>, Point> mapEntry) {
+    final int xMax = mapEntry.getRight().x;
+    final int yMax = mapEntry.getRight().y;
+    final var map = mapEntry.getLeft();
     for (int i = 1; i <= Math.max(xMax, yMax); i++) {
       if (i <= xMax && verticalTest(i, map, xMax)) {
         return i;
@@ -62,9 +65,10 @@ public class Day13 {
     return 0;
   }
 
-  private int smudgeReflectionScore(final Set<Point> map) {
-    final int xMax = map.stream().mapToInt(p -> p.x).max().orElse(0);
-    final int yMax = map.stream().mapToInt(p -> p.y).max().orElse(0);
+  private int smudgeReflectionScore(final Pair<Set<Point>, Point> mapEntry) {
+    final var xMax = mapEntry.getRight().x;
+    final var yMax = mapEntry.getRight().y;
+    final var map = mapEntry.getLeft();
     for (int i = 1; i <= Math.max(xMax, yMax); i++) {
       if (i <= xMax && verticalSmudgeTest(i, map, xMax)) {
         return i;
@@ -99,15 +103,12 @@ public class Day13 {
       final int xMax) {
     final Set<Point> left = new HashSet<>();
     final Set<Point> rightReflected = new HashSet<>();
+    final var boundaries = setBoundaries(xTest, xMax);
 
-    final var leftMinBound = xTest <= xMax / 2 ? 0 : 2 * xTest - xMax - 1;
-    final var leftMaxBound = xTest - 1;
-    final var rightMinBound = xTest;
-    final var rightMaxBound = xTest <= xMax / 2 ? xTest * 2 - 1 : xMax;
     for (final var point : map) {
-      if (leftMinBound <= point.x && point.x <= leftMaxBound) {
+      if (boundaries[0] <= point.x && point.x <= boundaries[1]) {
         left.add(point);
-      } else if (rightMinBound <= point.x && point.x <= rightMaxBound) {
+      } else if (boundaries[2] <= point.x && point.x <= boundaries[3]) {
         rightReflected.add(new Point((2 * xTest - point.x) - 1, point.y));
       }
     }
@@ -118,19 +119,25 @@ public class Day13 {
       final int yMax) {
     final Set<Point> top = new HashSet<>();
     final Set<Point> bottomReflected = new HashSet<>();
+    final var boundaries = setBoundaries(yTest, yMax);
 
-    final var topMinBound = yTest <= yMax / 2 ? 0 : 2 * yTest - yMax - 1;
-    final var topMaxBound = yTest - 1;
-    final var bottomMinBound = yTest;
-    final var bottomMaxBound = yTest <= yMax / 2 ? yTest * 2 - 1 : yMax;
     for (final var point : map) {
-      if (topMinBound <= point.y && point.y <= topMaxBound) {
+      if (boundaries[0] <= point.y && point.y <= boundaries[1]) {
         top.add(point);
-      } else if (bottomMinBound <= point.y && point.y <= bottomMaxBound) {
+      } else if (boundaries[2] <= point.y && point.y <= boundaries[3]) {
         bottomReflected.add(new Point(point.x, (2 * yTest - point.y) - 1));
       }
     }
 
     return Pair.of(top, bottomReflected);
+  }
+
+  private int[] setBoundaries(final int test, final int max) {
+    final int[] boundaries = new int[4];
+    boundaries[0] = test <= (max / 2) ? 0 : ((2 * test) - max) - 1;
+    boundaries[1] = test - 1;
+    boundaries[2] = test;
+    boundaries[3] = test <= (max / 2) ? (test * 2) - 1 : max;
+    return boundaries;
   }
 }
